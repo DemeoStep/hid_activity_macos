45771f3335385fa9d1366ee06311d579fdb9929f
DemeoStep <demeo.alex@gmail.com>
2022-07-04 15:57:17 +0300

    remove Permissions Notifier

--- macos/Classes/ActivityStreamHandler.swift
+++ macos/Classes/ActivityStreamHandler.swift
@@ -5,18 +5,11 @@ class ActivityStreamHandler: NSObject, FlutterStreamHandler{
     var sink: FlutterEventSink?
 
     var timer: Timer?
-    
-//     let accessEnabled = AXIsProcessTrustedWithOptions(
-//       [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true] as CFDictionary)
 
     func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         
         sink = events
-        
-//         if accessEnabled != true {
-//           sink!("You need to enable the keylogger in the System Prefrences")
-//         }
-        
+
         NSEvent.addGlobalMonitorForEvents(matching: NSEvent.EventTypeMask.any, handler: { [self](event: NSEvent) in
             do {
                 let arr = event.description.components(separatedBy: " ")
