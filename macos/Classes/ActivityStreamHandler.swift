03cb7a8504c12d4fc3ca69bdd8a431fab6cd65e7
DemeoStep <demeo.alex@gmail.com>
2022-07-05 14:42:03 +0300

    feat: prompt Permissions if not granted, start monitoring only after permissions granted

--- macos/Classes/ActivityStreamHandler.swift
+++ macos/Classes/ActivityStreamHandler.swift
@@ -1,75 +1,73 @@
 import Foundation
 import FlutterMacOS
+import AppKit
 
 class ActivityStreamHandler: NSObject, FlutterStreamHandler{
     var sink: FlutterEventSink?
-
     var timer: Timer?
-
-    //TODO Check permissions periodically until not granted
     
-    let accessEnabled = AXIsProcessTrustedWithOptions(
-    [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true] as CFDictionary)
+    func startMonitors() {
+        NSEvent.addGlobalMonitorForEvents(matching: NSEvent.EventTypeMask.any, handler: { [self](event: NSEvent) in
+                do {
+                    let arr = event.description.components(separatedBy: " ")
+                    
+                    switch arr[1] {
+                    case ("type=KeyDown"):
+                        sink!("Keyboard key pressed")
+                    case ("type=KeyUp"):
+                        sink!("Keyboard key pressed")
+                    case ("type=SysDefined"):
+                        sink!("Keyboard key pressed")
+                    default:
+                        sink!("Mouse activity")
+                    }
+            }
+        })
+        
+        NSEvent.addLocalMonitorForEvents(matching: NSEvent.EventTypeMask.any, handler: { [self](event: NSEvent) in
+                do {
+                    let arr = event.description.components(separatedBy: " ")
 
+                    switch arr[1] {
+                    case "type=KeyDown":
+                        sink!("Keyboard key pressed")
+                    case ("type=KeyUp"):
+                        sink!("Keyboard key pressed")
+                    case ("type=SysDefined"):
+                        sink!("Keyboard key pressed")
+                    default:
+                        sink!("Mouse activity")
+                    }
+            }
+            
+            return event
+            
+        })
+    }
 
     func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         
         sink = events
         
-        if accessEnabled != true {
-                    sink!("You need to enable the keylogger in the System Prefrences")
-        } else {
-
-        NSEvent.addGlobalMonitorForEvents(matching: NSEvent.EventTypeMask.any, handler: { [self](event: NSEvent) in
-            do {
-                let arr = event.description.components(separatedBy: " ")
-                
-                switch arr[1] {
-                case ("type=KeyDown"):
-                    sink!("Keyboard key pressed")
-                case ("type=KeyUp"):
-                    sink!("Keyboard key pressed")
-                case ("type=SysDefined"):
-                    sink!("Keyboard key pressed")
-                default:
-                    sink!("Mouse activity")
+        if(AXIsProcessTrusted() == false) {
+            AXIsProcessTrustedWithOptions(
+                    [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true] as CFDictionary)
+            self.sink!("You need to enable the keylogger in the System Preferences")
+            if #available(macOS 10.12, *) {
+                timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {timer in
+                    if (AXIsProcessTrusted() == true) {
+                        timer.invalidate()
+                        self.startMonitors()
+                    }
                 }
-            
-            
+            }
         }
-            
-        })
-        
-        NSEvent.addLocalMonitorForEvents(matching: NSEvent.EventTypeMask.any, handler: { [self](event: NSEvent) in
-            do {
-                let arr = event.description.components(separatedBy: " ")
-                
-                switch arr[1] {
-                case "type=KeyDown":
-                    sink!("Keyboard key pressed")
-                case ("type=KeyUp"):
-                    sink!("Keyboard key pressed")
-                case ("type=SysDefined"):
-                    sink!("Keyboard key pressed")
-                default:
-                    sink!("Mouse activity")
-                    //sink!("\(arr[1])")
-                }
-            
-            
-        }
-            return event
-            
-        })
-        }
-        
         
         return nil
     }
 
     func onCancel(withArguments arguments: Any?) -> FlutterError? {
         sink = nil
-        timer?.invalidate()
         return nil
     }
 }
